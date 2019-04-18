5fcf6687c8ce391be8e75a36e14b80bd891d6425
Softwarica College Lab <softwarica@softwarica.com>
2019-04-18 11:31:14 +0545

    circleimageview

---
+++ app/src/main/java/ContactAdapter/ContactsAdapter.java
@@ -0,0 +1,25 @@
+package ContactAdapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.recyclerview.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ContactsAdapter {
+    public class ContactsViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView imgProfile;
+        TextView tvName, tvPhone;
+        public ContactsViewHolder (@NonNull View itemView) {
+            super(itemView);
+            imgProfile = itemView.findViewById(R.id.imgProfile);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvPhone = itemView.findViewById(R.id.tvPhone);
+
+        }
+    }
+}
