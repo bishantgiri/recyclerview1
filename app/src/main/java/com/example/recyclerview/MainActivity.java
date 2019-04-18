965f72e3f32812f28bcf1a06a250387076ab48ac
Softwarica College Lab <softwarica@softwarica.com>
2019-04-18 12:04:58 +0545

    displayed in linear layout

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ContactAdapter.ContactsAdapter;
+import model.Contacts;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        List<Contacts> contactsList = new ArrayList<>();
+        contactsList.add( new Contacts("Saugat Malla", "9808011122",R.drawable.saugat));
+        contactsList.add( new Contacts("Rajesh Hamal", "9808022122",R.drawable.rajesh));
+        contactsList.add( new Contacts("Dayahang Rai", "9808033122",R.drawable.dahayang));
+        contactsList.add( new Contacts("Bhuwan KC", "9808023122",R.drawable.bhuwan));
+
+        ContactsAdapter contactsAdapter = new ContactsAdapter(this, contactsList);
+        recyclerView.setAdapter(contactsAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
